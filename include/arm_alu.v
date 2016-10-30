d16f2bfaab77a858ca694d40ae00698d4cc4208d
Jai Luthra <me@jailuthra.in>
2016-10-30 05:38:42 +0530

    alu: use non-blocking statements, add default case

--- include/arm_alu.v
+++ include/arm_alu.v
@@ -8,15 +8,16 @@ module arm_alu
     /* FIXME: Not checking carry bit rn */
     always @ (*) begin
         case (alu_op_sel)
-            `AND, `TST:       alu_out = alu_op1 & alu_op2;
-            `EOR, `TEQ:       alu_out = alu_op1 ^ alu_op2;
-            `SUB, `SBC, `CMP: alu_out = alu_op1 - alu_op2;
-            `RSB, `RSC:       alu_out = alu_op2 - alu_op1;
-            `ADD, `ADC, `CMN: alu_out = alu_op1 + alu_op2;
-            `ORR:             alu_out = alu_op1 | alu_op2;
-            `MOV:             alu_out = alu_op2;
-            `BIC:             alu_out = alu_op1 & ~alu_op2;
-            `MVN:             alu_out = ~alu_op2;
+            `AND, `TST:       alu_out <= alu_op1 & alu_op2;
+            `EOR, `TEQ:       alu_out <= alu_op1 ^ alu_op2;
+            `SUB, `SBC, `CMP: alu_out <= alu_op1 - alu_op2;
+            `RSB, `RSC:       alu_out <= alu_op2 - alu_op1;
+            `ADD, `ADC, `CMN: alu_out <= alu_op1 + alu_op2;
+            `ORR:             alu_out <= alu_op1 | alu_op2;
+            `MOV:             alu_out <= alu_op2;
+            `BIC:             alu_out <= alu_op1 & ~alu_op2;
+            `MVN:             alu_out <= ~alu_op2;
+            default:          alu_out <= 32'bx;
         endcase
     end
 endmodule
