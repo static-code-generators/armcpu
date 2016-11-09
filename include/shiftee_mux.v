dfb5d3f1c9ec1b2bce1e2c3b7ccb9e3187975309
RadhikaG <radhikaghosal@gmail.com>
2016-11-10 00:17:32 +0530

    barrel: added shifter_mux and shiftee_mux

---
+++ include/shiftee_mux.v
@@ -0,0 +1,20 @@
+module shiftee_mux
+(
+    input         sel,
+    input  [7:0]  immed_8,
+    input  [31:0] rm,
+    output [31:0] shiftee
+);
+
+    always @(*) begin
+        case (sel)
+            1'b0: begin
+                shiftee <= {{24{immed_8[7]}}, immed_8};
+            end
+            1'b1: begin
+                shiftee <= rm;
+            end
+        endcase
+    end
+
+endmodule
