bdb24ca883d5e5dee0b675c2b4c854683bdce773
abrolmrinal <mrinal15062@iiitd.ac.in>
2016-11-11 03:40:34 +0530

    cond_decode: yah this compiles

--- include/cond_decode.v
+++ include/cond_decode.v
@@ -5,12 +5,32 @@
  */
 module cond_decode
 (
-    // inputs
     input [31:0] inst, cpsr,
-    // outputs
-    output valid
+    output reg valid
 );
+    wire [3:0] cond;
 
-    // Stuff in the cases here
+    assign cond = inst[31-:4]; //wired to condition bits in instruction
+
+    always @ (inst) begin
+        case(cond)
+            `EQ : valid <=  cpsr[`Z_BIT];
+            `NE : valid <= ~cpsr[`Z_BIT];
+            `CS : valid <=  cpsr[`C_BIT]; 
+            `CC : valid <= ~cpsr[`C_BIT];
+            `MI : valid <=  cpsr[`N_BIT];
+            `PL : valid <= ~cpsr[`N_BIT];
+            `VS : valid <=  cpsr[`V_BIT];
+            `VC : valid <= ~cpsr[`V_BIT];
+            `HI : valid <= (cpsr[`C_BIT]) && (~cpsr[`Z_BIT]);
+            `LS : valid <= (~cpsr[`C_BIT]) || (cpsr[`Z_BIT]);
+            `GE : valid <= ((cpsr[`N_BIT]) && (cpsr[`V_BIT])) || ((~cpsr[`N_BIT]) && (~cpsr[`V_BIT]));
+            `LT : valid <= ((~cpsr[`N_BIT]) && (cpsr[`V_BIT])) || ((cpsr[`N_BIT]) && (~cpsr[`V_BIT]));
+            `GT : valid <= (~cpsr[`Z_BIT]) && (((cpsr[`N_BIT]) && (cpsr[`V_BIT])) || ((~cpsr[`N_BIT]) && (~cpsr[`V_BIT])));
+            `LE : valid <= (cpsr[`Z_BIT]) && (((~cpsr[`N_BIT]) && (cpsr[`V_BIT])) || ((cpsr[`N_BIT]) && (~cpsr[`V_BIT])));
+            `AL : valid <= 1'b1;
+            default : valid <= 1'b0;
+        endcase
+    end
 
 endmodule
