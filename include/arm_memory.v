1d883c0523e7f5357fbc342d1da454f6373327b7
Jai Luthra <me@jailuthra.in>
2016-11-17 04:59:55 +0530

    memauri: wew looks like it's working

--- include/arm_memory.v
+++ include/arm_memory.v
@@ -1,133 +1,97 @@
+`define NB_REGIONS     2
+`define MEM_DATA       0
+`define MEM_TEXT       1
+`define MEM_DATA_START 32'h10000000
+`define MEM_DATA_SIZE  32'h00000100 // 256 bytes, 64 words
+`define MEM_DATA_TOP   `MEM_DATA_START + `MEM_DATA_SIZE
+`define MEM_TEXT_START 32'h00000000
+`define MEM_TEXT_SIZE  32'h00000100
+`define MEM_TEXT_TOP   `MEM_TEXT_START + `MEM_TEXT_SIZE
+
 module arm_memory
-#(
-    parameter NOF_BYTES = 2048,
-    // Text segment
-    parameter data_start = 32'h00000000,
-    parameter data_words = 32'h00000100, // 256
-    parameter data_length = data_words * 4,
-    parameter data_top = data_start + data_length,
-
-    // Data segment
-    parameter text_start = 32'h00100000,
-    parameter text_words = 32'h00000100, // 256
-    parameter text_length = text_words * 4,
-    parameter text_top = text_start + text_length
-)
 (
-    // Inputs
-    input clk, rst,
-    // Two-port memory
-    input [31:0] addr1,
-    input [31:0] addr2,
-    input [31:0] data_in1,
-    input [31:0] data_in2,
-    input write1,
-    input write2,
-    // Outputs
-    output reg excpt,
-    output reg [31:0] data_out1,
-    output reg [31:0] data_out2
+    input                  clk,
+    input      [0:1][31:0] addr,
+    input      [0:1][31:0] data_in,
+    input            [0:1] we,           // write enable
+    output reg       [0:1] excpt,        // exception bit
+    output reg [0:1][31:0] data_out
 );
+    reg region_sel [0:1];
+    reg [31:0] offset [0:1];
+
+    // loop variable for two-port memory
+    integer i;
+
     // Memory regions
-    reg [7:0] data_region[data_length:0];
-    reg [7:0] text_region[text_length:0];
+    reg [7:0] data_region[0 : `MEM_DATA_SIZE - 1];
+    reg [7:0] text_region[0 : `MEM_TEXT_SIZE - 1];
 
-    wire [31:0] offset1, offset2;
-
-    assign offset1 = CALC_OFFSET(addr1);
-    assign offset2 = CALC_OFFSET(addr2);
-
-    // Reading is asychronous
-    always @(*) begin
-        // Port 1
-        if (!write1) begin
-            if ((addr1 >= data_start) && (addr1 < data_top))
-                data_out1 = (data_region[offset1 + 0] << 24) |
-                            (data_region[offset1 + 1] << 16) |
-                            (data_region[offset1 + 2] <<  8) |
-                            (data_region[offset1 + 3] <<  0); 
-            else if ((addr1 >= text_start) && (addr1 < text_top))
-                data_out1 = (text_region[offset1 + 0] << 24) |
-                            (text_region[offset1 + 1] << 16) |
-                            (text_region[offset1 + 2] <<  8) |
-                            (text_region[offset1 + 3] <<  0);
+    // Combinatorial reads and address decoding
+    always @ (*) begin
+        for (i = 0; i < 2; i = i + 1) begin
+            ADDR_DECODE(offset[i], region_sel[i], excpt[i], addr[i]);
+            if (!we[i] && !excpt[i]) begin
+                if (region_sel[i] == `MEM_DATA) begin
+                    data_out[i] = (data_region[offset[i] + 0] << 24) |
+                                  (data_region[offset[i] + 1] << 16) |
+                                  (data_region[offset[i] + 2] <<  8) |
+                                  (data_region[offset[i] + 3] <<  0); 
+                end
+                else begin
+                    data_out[i] = (text_region[offset[i] + 0] << 24) |
+                                  (text_region[offset[i] + 1] << 16) |
+                                  (text_region[offset[i] + 2] <<  8) |
+                                  (text_region[offset[i] + 3] <<  0); 
+                end
+            end
             else
-                excpt = 1; // no region; we're daed
+                data_out[i] = 32'bx;
         end
-        else
-            data_out1 = 32'hxxxxxxxx;
-
-        // Port 2
-        if (!write2) begin
-            if ((addr1 >= data_start) && (addr1 < data_top))
-                data_out2 = (data_region[offset2 + 0] << 24) |
-                            (data_region[offset2 + 1] << 16) |
-                            (data_region[offset2 + 2] <<  8) |
-                            (data_region[offset2 + 3] <<  0); 
-            else if ((addr2 >= text_start) && (addr2 < text_top))
-                data_out2 = (text_region[offset2 + 0] << 24) |
-                            (text_region[offset2 + 1] << 16) |
-                            (text_region[offset2 + 2] <<  8) |
-                            (text_region[offset2 + 3] <<  0);
-            else
-                excpt = 1;
-        end
-        else
-            data_out2 = 32'hxxxxxxxx;
     end
 
-    // Writing is clocked at posedge
-    always @(posedge clk) begin
-        // Port 1
-        if (write1) begin
-            if ((addr1 >= data_start) && (addr1 < data_top)) begin
-                data_region[offset1 + 0] = (data_in1 >> 24) & 8'hFF; 
-                data_region[offset1 + 1] = (data_in1 >> 16) & 8'hFF; 
-                data_region[offset1 + 2] = (data_in1 >>  8) & 8'hFF; 
-                data_region[offset1 + 3] = (data_in1 >>  0) & 8'hFF; 
+    // Sequential writes
+    always @ (posedge clk) begin
+        for (i = 0; i < 2; i = i + 1) begin
+            if (we[i] && !excpt[i]) begin
+                if (region_sel[i] == `MEM_DATA) begin
+                    data_region[offset[i] + 0] = (data_in[i] >> 24) & 8'hFF; 
+                    data_region[offset[i] + 1] = (data_in[i] >> 16) & 8'hFF; 
+                    data_region[offset[i] + 2] = (data_in[i] >>  8) & 8'hFF; 
+                    data_region[offset[i] + 3] = (data_in[i] >>  0) & 8'hFF; 
+                end
+                else begin
+                    text_region[offset[i] + 0] = (data_in[i] >> 24) & 8'hFF; 
+                    text_region[offset[i] + 1] = (data_in[i] >> 16) & 8'hFF; 
+                    text_region[offset[i] + 2] = (data_in[i] >>  8) & 8'hFF; 
+                    text_region[offset[i] + 3] = (data_in[i] >>  0) & 8'hFF; 
+                end
             end
-            else if ((addr1 >= text_start) && (addr1 < text_top)) begin
-                text_region[offset1 + 0] = (data_in1 >> 24) & 8'hFF; 
-                text_region[offset1 + 1] = (data_in1 >> 16) & 8'hFF; 
-                text_region[offset1 + 2] = (data_in1 >>  8) & 8'hFF; 
-                text_region[offset1 + 3] = (data_in1 >>  0) & 8'hFF;
-            end
-            else
-                excpt = 1;
         end
-        else
-            data_out1 = 32'hxxxxxxxx;
-
-        // Port 2
-        if (write2) begin
-            if ((addr2 >= data_start) && (addr2 < data_top)) begin
-                data_region[offset2 + 0] = (data_in2 >> 24) & 8'hFF; 
-                data_region[offset2 + 1] = (data_in2 >> 16) & 8'hFF; 
-                data_region[offset2 + 2] = (data_in2 >>  8) & 8'hFF; 
-                data_region[offset2 + 3] = (data_in2 >>  0) & 8'hFF; 
-            end
-            else if ((addr2 >= text_start) && (addr2 < text_top)) begin
-                text_region[offset2 + 0] = (data_in2 >> 24) & 8'hFF; 
-                text_region[offset2 + 1] = (data_in2 >> 16) & 8'hFF; 
-                text_region[offset2 + 2] = (data_in2 >>  8) & 8'hFF; 
-                text_region[offset2 + 3] = (data_in2 >>  0) & 8'hFF;
-            end
-            else
-                excpt = 1;
-        end
-        else
-            data_out2 = 32'hxxxxxxxx;
     end
 
-    function [31:0] CALC_OFFSET
+    // Address decoder
+    task ADDR_DECODE
     (
-        input [29:0] addr
+        output reg [31:0] offset,
+        output reg region_sel,
+        output reg excpt, 
+        input [31:0] addr
     );
-        if ((addr >= data_start) && (addr < data_top))
-            CALC_OFFSET = addr - data_start;
-        else if ((addr >= text_start) && (addr < text_top))
-            CALC_OFFSET = addr - text_start;
-        else
-            CALC_OFFSET = 32'bx;
-    endfunction
+        if ((addr >= `MEM_DATA_START) && (addr < `MEM_DATA_TOP)) begin
+            offset     = addr - `MEM_DATA_START;
+            region_sel = `MEM_DATA;
+            excpt      = 0;
+        end
+        else if ((addr >= `MEM_TEXT_START) && (addr < `MEM_TEXT_TOP)) begin
+            offset     = addr - `MEM_TEXT_START;
+            region_sel = `MEM_TEXT;
+            excpt      = 0;
+        end
+        else begin
+            offset     = 32'bx;
+            region_sel = 2'bx;
+            excpt      = 1;
+        end
+    endtask
 endmodule
