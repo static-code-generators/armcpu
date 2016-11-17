32320711c13fe2ecd4b117c0170be28d97d223c1
parthmittal <parth15069@iiitd.ac.in>
2016-11-17 21:29:24 +0530

    testbenches: great reindent event of shame

--- testbenches/tb_register_file.v
+++ testbenches/tb_register_file.v
@@ -3,52 +3,52 @@
 `define ADDR_WIDTH 4
 
 module tb_register_file;
-	reg clk, reset;
-	reg rd_we;
-	reg [`WORD_SIZE - 1:0] rd_in;
-	reg [`ADDR_WIDTH - 1:0] write_rd;
-	reg [`ADDR_WIDTH - 1:0] read_rn, read_rm;
-	reg [`WORD_SIZE - 1:0] pc_in, cpsr_in;
-	reg pc_we, cpsr_we;
-	
-	wire [`WORD_SIZE - 1:0] rn_out, rm_out;
-	wire [`WORD_SIZE - 1:0] pc_out, cpsr_out;
-	integer i;
+    reg clk, reset;
+    reg rd_we;
+    reg [`WORD_SIZE - 1:0] rd_in;
+    reg [`ADDR_WIDTH - 1:0] write_rd;
+    reg [`ADDR_WIDTH - 1:0] read_rn, read_rm;
+    reg [`WORD_SIZE - 1:0] pc_in, cpsr_in;
+    reg pc_we, cpsr_we;
 
-	initial begin
-		clk = 0;
-		reset = 1;
-		rd_we = 0;
-		rd_in = 0;
-		write_rd = 0;
-		read_rn = 0;
-		read_rm = 0;
-		pc_in = 0;
-		cpsr_in = 0;
-		pc_we = 0;
-		cpsr_we = 0;
+    wire [`WORD_SIZE - 1:0] rn_out, rm_out;
+    wire [`WORD_SIZE - 1:0] pc_out, cpsr_out;
+    integer i;
 
-		#2 reset = 0;
-		#2 rd_we = 1;
-		#2 rd_in = 42;
+    initial begin
+        clk = 0;
+        reset = 1;
+        rd_we = 0;
+        rd_in = 0;
+        write_rd = 0;
+        read_rn = 0;
+        read_rm = 0;
+        pc_in = 0;
+        cpsr_in = 0;
+        pc_we = 0;
+        cpsr_we = 0;
 
-		for (i = 0; i < `NUM_REGS; i = i + 1) begin
-			#5
-			read_rn = i; //we aren't reading rm for now.
-			write_rd = i;
-		end
+        #2 reset = 0;
+        #2 rd_we = 1;
+        #2 rd_in = 42;
 
-		#2 $finish;
-	end
+        for (i = 0; i < `NUM_REGS; i = i + 1) begin
+            #5
+            read_rn = i; //we aren't reading rm for now.
+            write_rd = i;
+        end
 
-	always begin
-		#1 clk = clk ^ 1;
-	end
+        #2 $finish;
+    end
+
+    always begin
+        #1 clk = clk ^ 1;
+    end
 
     initial begin
         $monitor("clk: %b, reset: %b, rd_we: %b, write_rd: %d, rd_in: %d, read_rn: %d, rn_out: %d",
-			clk, reset, rd_we, write_rd, rd_in, read_rn, rn_out);
+            clk, reset, rd_we, write_rd, rd_in, read_rn, rn_out);
     end
 
-	register_file reg_file(clk, reset, rd_we, rd_in, write_rd, read_rn, read_rm, pc_in, cpsr_in, pc_we, cpsr_we, rn_out, rm_out, pc_out, cpsr_out);
+    register_file reg_file(clk, reset, rd_we, rd_in, write_rd, read_rn, read_rm, pc_in, cpsr_in, pc_we, cpsr_we, rn_out, rm_out, pc_out, cpsr_out);
 endmodule
--- testbenches/tb_shifter_mux.v
+++ testbenches/tb_shifter_mux.v
@@ -1,68 +1,68 @@
 module tb_shifter_mux;
-	reg[1:0] sel;
-	reg[3:0] rotate_imm;
-	reg[4:0] shift_imm;
-	reg[31:0] rs;
-	wire[31:0] shifter;
+    reg[1:0] sel;
+    reg[3:0] rotate_imm;
+    reg[4:0] shift_imm;
+    reg[31:0] rs;
+    wire[31:0] shifter;
 
-	integer i;
+    integer i;
 
-	initial begin
-		sel = 0;
-		rotate_imm = 0;
-		shift_imm = 0;
-		rs = 0;
+    initial begin
+        sel = 0;
+        rotate_imm = 0;
+        shift_imm = 0;
+        rs = 0;
 
-		#5;
+        #5;
 
-		for (i = 0; i < (1 << 4); i = i + 1) begin
-			#1 rotate_imm = i;
-		end
+        for (i = 0; i < (1 << 4); i = i + 1) begin
+            #1 rotate_imm = i;
+        end
 
-		#5;
-		sel = 1;
+        #5;
+        sel = 1;
 
-		for (i = 0; i < (1 << 5); i = i + 1) begin
-			#1 shift_imm = i;
-		end
+        for (i = 0; i < (1 << 5); i = i + 1) begin
+            #1 shift_imm = i;
+        end
 
-		#5;
-		sel = 2;
+        #5;
+        sel = 2;
 
-		for (i = 0; i < 32; i = i + 1) begin
-			#1 rs = rs | (1 << i);
-		end
+        for (i = 0; i < 32; i = i + 1) begin
+            #1 rs = rs | (1 << i);
+        end
 
-		#5;
-		$display("all ok");
-		$finish;
-	end
-	
-	always @ (shifter) begin
-		case (sel)
-			2'b00: begin
-				if (shifter != {{28{rotate_imm[3]}}, rotate_imm}) begin
-					$display("bad bad | sel: %b, rotate_imm: %b, shifter: %b",
-						sel, rotate_imm, shifter);
-					$stop;
-				end
-			end
-			2'b01: begin
-				if (shifter != {{27{shift_imm[4]}}, shift_imm}) begin
-					$display("bad bad | sel: %b, shift_imm: %b, shifter: %b",
-						sel, shift_imm, shifter);
-					$stop;
-				end
-			end
-			2'b10: begin
-				if (shifter != rs) begin
-					$display("bad bad | sel: %b, rs: %d, shifter: %d",
-						sel, rs, shifter);
-					$stop;
-				end
-			end
-		endcase
-	end
+        #5;
+        $display("all ok");
+        $finish;
+    end
 
-	shifter_mux smux(sel, rotate_imm, shift_imm, rs, shifter);
+    always @ (shifter) begin
+        case (sel)
+            2'b00: begin
+                if (shifter != {{28{rotate_imm[3]}}, rotate_imm}) begin
+                    $display("bad bad | sel: %b, rotate_imm: %b, shifter: %b",
+                        sel, rotate_imm, shifter);
+                    $stop;
+                end
+            end
+            2'b01: begin
+                if (shifter != {{27{shift_imm[4]}}, shift_imm}) begin
+                    $display("bad bad | sel: %b, shift_imm: %b, shifter: %b",
+                        sel, shift_imm, shifter);
+                    $stop;
+                end
+            end
+            2'b10: begin
+                if (shifter != rs) begin
+                    $display("bad bad | sel: %b, rs: %d, shifter: %d",
+                        sel, rs, shifter);
+                    $stop;
+                end
+            end
+        endcase
+    end
+
+    shifter_mux smux(sel, rotate_imm, shift_imm, rs, shifter);
 endmodule
